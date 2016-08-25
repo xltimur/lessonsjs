3402d6aaf44bb303f77b4993322247a7b8056249
Alex <alex@Alexs-MacBook-Pro-2.local>
2016-08-26 01:33:40 +0300

    calc

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -15,19 +15,32 @@
 //    console.log( array[--i] );
 //}
 
-//-------Функции------
+//-------calc-----
 
-var array = [1,2,3,4,5,6,7,8,9,10];
-for (
-    var i = 0;
-    i < array.length; i++) {
-    console.log( array [i] );
+/*----число-----*/
+
+function Sum(a,b){
+
+    if (!isNaN(parseFloat(a,b)) && isFinite(a,b))
+        console.log('число');
+    else
+        console.log('не число!');
+    var c = a + b;
+
+    return c;
 }
+console.log(Sum(200,100));
 
+/*----не число----*/
 
-var array = [1,2,3,4,5,6,7,8,9,10];
-for (
-    var i = array.length;
-    i > 0; --i){
-    console.log( array[--i] );
+function Sum(a,b){
+
+    if (!isNaN(parseFloat(a,b)) && isFinite(a,b))
+        console.log('число');
+    else
+        console.log('не число!');
+    var c = a + b;
+
+    return c;
 }
+console.log(Sum('ааа',100));
