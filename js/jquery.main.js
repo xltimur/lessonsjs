6401cc547963b5b65d57f22dba1d35e5ebcfef7a
Alex <alex@Alexs-MacBook-Pro-2.local>
2016-08-26 01:20:22 +0300

    создал калькулбкулятор

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -14,3 +14,20 @@ for (
     i > 0; --i){
     console.log( array[--i] );
 }
+
+//-------Функции------
+
+var array = [1,2,3,4,5,6,7,8,9,10];
+for (
+    var i = 0;
+    i < array.length; i++) {
+    console.log( array [i] );
+}
+
+
+var array = [1,2,3,4,5,6,7,8,9,10];
+for (
+    var i = array.length;
+    i > 0; --i){
+    console.log( array[--i] );
+}
