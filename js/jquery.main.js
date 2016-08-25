cf89ee5d760810eaa4fe3f56b3ecb19302400ba0
Alex <alex@Alexs-MacBook-Pro-2.local>
2016-08-26 01:10:19 +0300

    создал масивы

---
+++ js/jquery.main.js
@@ -0,0 +1,16 @@
+//---------Масивы---------
+
+var array = [1,2,3,4,5,6,7,8,9,10];
+for (
+    var i = 0;
+    i < array.length; i++) {
+    console.log( array [i] );
+}
+
+
+var array = [1,2,3,4,5,6,7,8,9,10];
+for (
+    var i = array.length;
+    i > 0; --i){
+    console.log( array[--i] );
+}
