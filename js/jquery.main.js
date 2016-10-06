83b28021a7c44a5a150b8309a4000503d176f168
xltimur <xltimur>
2016-10-06 12:03:33 +0300

    Анимация списка

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -68,16 +68,55 @@
 //}
 
 
-shoveDate();
-function shoveDate(){
-    setInterval(function(){
-        console.clear();
-        var time = new Date();
-        var h = time.getHours();
-        var m = time.getMinutes();
-        var s = time.getSeconds();
-        if (s < 10) s = '0' + s;
-        var fullTime = h + ':' + m + ':' + s;
-        console.log(fullTime);
-    }, 1000);
-}
+//shoveDate();
+//function shoveDate(){
+//    setInterval(function(){
+//        console.clear();
+//        var time = new Date();
+//        var h = time.getHours();
+//        var m = time.getMinutes();
+//        var s = time.getSeconds();
+//        if (s < 10) s = '0' + s;
+//        var fullTime = h + ':' + m + ':' + s;
+//        console.log(fullTime);
+//    }, 1000);
+//}
+
+
+$(document).ready(function(){
+    /* инициализация функций */
+    /* описание функций */
+    $(".button").click(function () {
+        if ($("ul").is(":hidden")) {
+            $("ul").slideDown("slow");
+        } else {
+            $("ul").slideUp("slow");
+        }
+    });
+
+
+    $(".button2").click(function () {
+        if ($("ul").is(":hidden")) {
+            $("ul").fadeIn("slow");
+        } else {
+            $("ul").fadeOut("slow");
+        }
+    });
+
+    $(".button3").click(function () {
+        if ($("ul").is(":hidden")) {
+            $("ul").animate({
+                opacity: 1,
+                left: "+=50",
+                height: "toggle"
+            });
+        } else {
+            $("ul").animate({
+                opacity: 0,
+                left: "+=150",
+                height: "toggle"
+            });
+        }
+    });
+
+});
